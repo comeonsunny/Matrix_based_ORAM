680681196b3e6afdfaa3a62a6232d3947d11f730
comeonsunny <2286323940@qq.com>
2022-09-25 14:21:19 +0800

    From Client

---
+++ src/ClientMatrixORAM.hpp
@@ -0,0 +1,14 @@
+# ifndef ClientMatrixORAM_hpp
+# define ClientMatrixORAM_hpp
+#include "Config.hpp"
+#include <string>
+class ClientMatrixORAM {
+private:
+
+public:
+    ClientMatrixORAM();
+    ~ClientMatrixORAM();
+    int initialize();
+    int access(TYPE_INDEX blockID, string op);
+};
+# endif
---
+++ src/Config.hpp
@@ -0,0 +1,10 @@
+#ifndef Config_hpp
+#define Config_hpp
+
+//=== DATA TYPE ===============================================
+using TYPE_INDEX = unsigned long long;
+using TYPE_DATA = long long int;
+//=== Parameters ===============================================
+#define BLOCK_SIZE 16  // in bytes
+#define DB_SIZE 1024   // in bytes
+#endif
