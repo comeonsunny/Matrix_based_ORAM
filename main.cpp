4fef36237f221e5348ccfa23d3f8154154505a75
changqi <84911039+comeonsunny@users.noreply.github.com>
2022-09-27 16:12:06 +0800

    ModificationOfGlobalVariables

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <cmath>
-using TYPE_INDEX = unsigned long long;
-#define BLOCK_SIZE 5  // in bytes
-#define DB_SIZE 46   // in bytes
-const double REAL_BLOCK_NUM = ceil((double)DB_SIZE / BLOCK_SIZE);
-const TYPE_INDEX LENGTH_BLOCK_NUM = ceil(sqrt(REAL_BLOCK_NUM));
-int main()
-{
-    std::cout << REAL_BLOCK_NUM << std::endl;
-    // check the data type of REAL_BLOCK_NUM
-    std::cout << LENGTH_BLOCK_NUM << std::endl;
-    std::cout << (typeid(LENGTH_BLOCK_NUM) == typeid(TYPE_INDEX)) << std::endl;
+#include "src/Config.hpp"
+using namespace std;
+
+// a main function with arguments
+int main(int argc, char *argv[]) {
+    if (argc > 2) {
+        BLOCK_SIZE = atoi(argv[1]);
+        DB_SIZE = atoi(argv[2]);
+    }
+    cout << "BLOCK_SIZE: " << BLOCK_SIZE << endl;
+    cout << "DB_SIZE: " << DB_SIZE << endl;
     return 0;
 }
--- src/Config.hpp
+++ src/Config.hpp
@@ -1,15 +1,15 @@
 #ifndef Config_hpp
 #define Config_hpp
+
 #include <cmath>
 
 //=== DATA TYPE ===============================================
 using TYPE_INDEX = unsigned long long;
 using TYPE_DATA = long long int;
 //=== Parameters ===============================================
-#define BLOCK_SIZE 16  // in bytes
-#define DB_SIZE 1024   // in bytes
+TYPE_INDEX BLOCK_SIZE = 16;  // in bytes
+TYPE_INDEX DB_SIZE = 1024;  // in bytes
 const TYPE_INDEX REAL_BLOCK_NUM = ceil((double)DB_SIZE / BLOCK_SIZE);
 const TYPE_INDEX LENGTH_BLOCK_NUM = ceil(sqrt(REAL_BLOCK_NUM));
 const TYPE_INDEX TOTAL_BLOCK_NUM = LENGTH_BLOCK_NUM * LENGTH_BLOCK_NUM;
-const 
 #endif
